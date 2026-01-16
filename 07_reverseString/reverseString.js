bbacc6056731c9afb04640d25fa4312c47c5671a
karenartc <karenrc.rrss@gmail.com>
2026-01-16 17:10:47 -0300

    exercise 07 reverse String done succesfully and all test passed.

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let arrayLetters = string.split('');
+    let reverseArray = arrayLetters.reverse();
+    let reverseString = reverseArray.join('');
+    return reverseString;
 };
 
 // Do not edit below this line
