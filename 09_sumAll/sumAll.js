7726b1eeae91efb1a71814e849229c3867f8e195
karenartc <karenrc.rrss@gmail.com>
2026-01-16 19:15:45 -0300

    exercise 09 sum All done succesfully and all test passed.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if((Number.isInteger(num1) && Number.isInteger(num2)) && (num1>0 && num2>0)){
+        let minorNumber = (num2>num1) ? num1 : num2;
+        let greaterNumber = (num2>num1) ? num2 : num1;
+        let sum = 0;
 
+        for(let i = minorNumber; i <= greaterNumber; i++){
+            let numberToSum = i;
+            sum += numberToSum;
+        }
+
+        return sum;
+    }else{
+        return('ERROR');
+    }
 };
+
+console.log(sumAll(1, 4));
 
 // Do not edit below this line
 module.exports = sumAll;
