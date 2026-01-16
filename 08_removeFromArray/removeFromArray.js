b68ec58f5e72ba0958a642d6c54f7acff515f2a5
karenartc <karenrc.rrss@gmail.com>
2026-01-16 18:01:12 -0300

    exercise 08 remove From Array done succesfully and all test passed.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    args.forEach(arg => {
+        let argumentIndex = array.indexOf(arg);
+        while (argumentIndex != -1){
+            array.splice(argumentIndex, 1);
+            argumentIndex = array.indexOf(arg);
+        }
+    }); 
+    return array;
 };
 
 // Do not edit below this line
