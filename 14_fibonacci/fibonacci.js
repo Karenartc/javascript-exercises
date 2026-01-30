7d6fe57699b1d254fbfce19946c0b824d554653e
karenartc <karenrc.rrss@gmail.com>
2026-01-30 19:35:12 -0300

    exercise 14 fibonacci done and all test passed

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let n1 = 1;
+    let n2 = 1;
+    let newN = 0;
 
+    if (n == 1 || n == 2){
+        return 1;
+    } else if (n < 0){
+        return 'OOPS';
+    }
+
+    for (let i = 2; i < n; i++){
+        newN = n1 + n2;
+
+        n1 = n2;
+        n2 = newN;
+    }
+
+    return newN;
 };
 
 // Do not edit below this line
