f4a920657a3a53340197522b73a5f14c058aadbb
karenartc <karenrc.rrss@gmail.com>
2026-01-30 18:33:45 -0300

    exercise 12 calculator done and all test passed

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,26 @@
-const add = function() {
-	
+const add = function(n1, n2) {
+	return n1 + n2;
 };
 
-const subtract = function() {
-	
+const subtract = function(n1, n2) {
+	return n1 - n2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((sum, num) => sum += num, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((mult, num) => mult *= num, 1)
 };
 
-const power = function() {
-	
+const power = function(n1, n2) {
+	return Math.pow(n1, n2);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if(n === 0 || n === 1) return 1;
+  return n * factorial(n - 1);
 };
 
 // Do not edit below this line
