244e75d83c8be9208c3c84f314bab093a7a57a8d
karenartc <karenrc.rrss@gmail.com>
2026-01-30 19:00:15 -0300

    exercise 13 palindromes done and all test passed

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (phrase) {
+    let cleanPhrase = phrase.replace(/[.,!¡¿\s]+/g, '').toLowerCase();
+    let reversed = Array.from(cleanPhrase).reverse().join('');
+    return (cleanPhrase === reversed);
 };
 
 // Do not edit below this line
